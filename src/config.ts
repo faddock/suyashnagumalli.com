0c09b40347cc4ac1bed84018e845367b83e030c2
faddock <usasuyash@gmail.com>
2025-07-17 03:35:30 +0530

    content: update resume filename to Jul 25 version 3

--- src/config.ts
+++ src/config.ts
@@ -17,5 +17,5 @@ export const SITE = {
     appendFilePath: true,
   },
   dynamicOgImage: true,
-  resume_filename: "Suyash_Nagumalli_Resume_Jul_25.pdf",
+  resume_filename: "Suyash_Nagumalli_Resume_Jul_25_3.pdf",
 } as const;
